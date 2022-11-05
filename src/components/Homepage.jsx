285c376f2e067207d32c40b8740814005f457e39
ayobamiseun <adegokeayobamiseun@gmail.com>
2022-11-05 08:08:05 +0100

    update

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import millify from 'millify'
 import { Typography, Row, Col, Statistic  } from 'antd'
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+import { useGetCryptosQuery } from '../services/CryptoApi';
 
 const {Title }= Typography;
 const Homepage = () => {
+
+    const {data, isFetching} = useGetCryptosQuery();
+    const globalStat = data?.data?.stats;
+    console.log(data);
+
   return (
     <>
       <Title level={2} className="heading">Global Crypto Stats</Title> 
       <Row gutter={[32, 32]}>
-        <Col span={12}><Statistic title="Total Cryptocurrencies"  /></Col>
-        <Col span={12}><Statistic title="Total Exchanges"  /></Col>
+        <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStat.total} /></Col>
+        <Col span={12}><Statistic title="Total Exchanges"  value={globalStat.totalExchanges} /></Col>
         <Col span={12}><Statistic title="Total Market Cap:" /></Col>
         <Col span={12}><Statistic title="Total 24h Volume"  /></Col>
         <Col span={12}><Statistic title="Total Cryptocurrencies" /></Col>
---
+++ src/services/CryptoApi.js
@@ -0,0 +1,41 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+
+
+const cryptoApiHeaders = {
+  'x-rapidapi-host': "coinranking1.p.rapidapi.com",
+  'x-rapidapi-key': 'a923b824cbmsh73480737b6dfb20p11cbdajsn6b3b1a04efa7',
+};
+ 
+const baseUrl = "https://coinranking1.p.rapidapi.com"
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl}),
+    endpoints: (builder) => ({
+      getCryptos: builder.query({
+        query: (count) => createRequest('/coins')
+      }),
+  
+    //   getCryptoDetails: builder.query({
+    //     query: (coinId) => createRequest(`/coin/${coinId}`),
+    //   }),
+  
+      // Note: Change the coin price history endpoint from this - `coin/${coinId}/history/${timeperiod} to this - `coin/${coinId}/history?timeperiod=${timeperiod}`
+    //   getCryptoHistory: builder.query({
+    //     query: ({ coinId, timeperiod }) => createRequest(`coin/${coinId}/history?timeperiod=${timeperiod}`),
+    //   }),
+  
+      // Note: To access this endpoint you need premium plan
+    //   getExchanges: builder.query({
+    //     query: () => createRequest('/exchanges'),
+    //   }),
+    }),
+  });
+  
+  export const {
+    useGetCryptosQuery
+    // useGetCryptoDetailsQuery,
+    // useGetExchangesQuery,
+    // useGetCryptoHistoryQuery,
+  } = cryptoApi;

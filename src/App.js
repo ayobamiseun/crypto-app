e89f4c77a56f9c46665a6c36773934f8b33adc6f
ayobamiseun <adegokeayobamiseun@gmail.com>
2022-11-06 15:49:22 +0100

    update

--- src/App.js
+++ src/App.js
@@ -33,9 +33,9 @@ const App = () => (
         </div>
       </Layout>
       <div className="footer">
-        <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2021
+        <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2022
           <Link to="/">
-            Cryptoverse Inc.
+            Ayobami Inc.
           </Link> <br />
           All Rights Reserved.
         </Typography.Title>
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,25 +3,39 @@ import millify from 'millify'
 import { Typography, Row, Col, Statistic  } from 'antd'
 import {Link} from 'react-router-dom';
 import { useGetCryptosQuery } from '../services/CryptoApi';
+import Cryptocurrencies from './Cryptocurrencies';
+import News from './News';
+// import Loader from './Loader';
 
 const {Title }= Typography;
 const Homepage = () => {
-
-    const {data, isFetching} = useGetCryptosQuery();
+  
+    const {data, isFetching} = useGetCryptosQuery(10);
     const globalStat = data?.data?.stats;
     console.log(data);
+    // if (isFetching || !globalStats) return <Loader />;
 
   return (
     <>
       <Title level={2} className="heading">Global Crypto Stats</Title> 
       <Row gutter={[32, 32]}>
-        <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStat.total} /></Col>
-        <Col span={12}><Statistic title="Total Exchanges"  value={globalStat.totalExchanges} /></Col>
-        <Col span={12}><Statistic title="Total Market Cap:" /></Col>
-        <Col span={12}><Statistic title="Total 24h Volume"  /></Col>
-        <Col span={12}><Statistic title="Total Cryptocurrencies" /></Col>
-        <Col span={12}><Statistic title="Total Markets" /></Col>
+        <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStat?.total} /></Col>
+        <Col span={12}><Statistic title="Total Exchanges"  value={millify(globalStat?.totalExchanges)} /></Col>
+        <Col span={12}><Statistic title="Total Market Cap:" value={millify(globalStat?.totalMarketCap)} /></Col>
+        <Col span={12}><Statistic title="Total 24h Volume"  value={millify(globalStat?.total24hVolume)}/></Col>
+        <Col span={12}><Statistic title="Total Cryptocurrencies" value={millify(globalStat?.totalCoins)} /></Col>
+        <Col span={12}><Statistic title="Total Markets" value={millify(globalStat?.totalMarkets)} /></Col>
       </Row>
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">Top 10 Cryptos In The World</Title>
+        <Title level={3} className="show-more"><Link to="/cryptocurrencies">Show more</Link></Title>
+      </div>
+      <Cryptocurrencies simplified />
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">Latest Crypto News</Title>
+        <Title level={3}><Link to="/news">Show more</Link></Title>
+      </div>
+      <News simplified />
     </>
   )
 }
